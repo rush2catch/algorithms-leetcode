4c063e510ab03defad817f3603b03ec43bcdec10
Yuan Jiang <jiangyuan1988@gmail.com>
2017-05-18 19:57:05 +0800

    parentheses parser can deal with '()' '[]' and {}'

--- stack/parChecker2.py
+++ stack/parChecker2.py
@@ -1,29 +1,29 @@
 from stack import Stack
 
-class Solution(object):
+#class Solution(object):
 
-    def parChecker2(symbolString):
-        s = Stack()
-        balanced = True
-        index = 0
-        while index < len(symbolString) and balanced:
-            symbol = symbolString[index]
-            if symbol in "({[":
-                s.push(symbol)
-            else:
-                if s.isEmpty():
-                    balanced = False
-                else:
-                    top = s.pop()
-                    if top != symbol:
-                        balanced = False
-            index = index + 1
-
-        if balanced and s.isEmpty():
-            return True
+def parChecker2(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol in "({[":
+            s.push(symbol)
         else:
-            return False
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if top != symbol:
+                    balanced = False
+        index = index + 1
 
-obj = Solution()
-print(obj.parChecker2('[](){}'))
-#print(obj.parChecker('[{()]'))
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+#obj = Solution()
+print(parChecker2('[][[[{](){}][}[['))
+print(parChecker2('()'))

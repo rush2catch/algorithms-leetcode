f467802ec6e6db7a272d493b1a2a2d3c7c84ed98
Yuan Jiang <jiangyuan1988@gmail.com>
2017-05-24 19:40:58 +0800

    # Dynamic Arrays

---
+++ Basic Data Structures/array/DynamicArray.py
@@ -0,0 +1,9 @@
+# provides getsizeof() function
+import sys
+
+data = []
+for k in range(10):
+    a = len(data)               # number of elements
+    b = sys.getsizeof(data)     # actual sizes in bytes
+    print("Length: {0:3d}; Size in bytes: {1:4d}".format(a, b))
+    data.append(None)           # increase size by None

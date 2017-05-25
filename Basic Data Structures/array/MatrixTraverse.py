e32fd782ce83f5331eab9d5a6628a6a5e984682e
Yuan Jiang <jiangyuan1988@gmail.com>
2017-05-25 19:05:02 +0800

    # leetcode 566 reshape the matrix

--- Basic Data Structures/array/MatrixTraverse.py
+++ Basic Data Structures/array/MatrixTraverse.py
@@ -3,6 +3,7 @@ given a matrix, convert it to a one row  matrix or list
 @:arg matrix
 """
 
+
 def matrix_traverse(matrix):
     if len(matrix) == 0:
         return None
@@ -12,5 +13,5 @@ def matrix_traverse(matrix):
             new_matrix.append(matrix[row][column])
     return new_matrix
 
-print(matrix_traverse([[1,2,3], [4,5,6]]))
-print(matrix_traverse([[1,2,3],[4,5,6],[7,8,9]]))
+print(matrix_traverse([[1, 2, 3], [4, 5, 6]]))
+print(matrix_traverse([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))

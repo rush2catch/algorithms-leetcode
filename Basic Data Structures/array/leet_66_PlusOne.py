f5b6801907344543134912b83ba7caddecce43e4
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-01 18:05:41 +0800

    leet_66 Plus One

--- Basic Data Structures/array/leet_66_PlusOne.py
+++ Basic Data Structures/array/leet_66_PlusOne.py
@@ -14,7 +14,7 @@ The digits are stored such that the most significant digit is at the head of the
 We're given a list of digits, and the idea here is to convert that list to an integer, num.
 So each digit is multiplied by the proper place value and added to num.
 For example, if digits = [3, 8, 2, 5] then on the first iteration 3 is multiplied by 10 to the power of 4-1-0 = 3,
-so this results in 3000, which is added to num. Then 8 is multiplied by 10^2 and added to num, and so on.
+so this results in 3000, which is added to num. Then 8 is multiplied by 10^2 and added to num, and so on.1
 
 The last step is to add 1 to num, convert it to a list and return that list.
 """

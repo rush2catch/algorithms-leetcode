5709ab46fd5ae516fe09eaac86d82888548b3e94
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-01 18:02:50 +0800

    bubble sort

--- Basic Data Structures/array/leet_66_PlusOne.py
+++ Basic Data Structures/array/leet_66_PlusOne.py
@@ -8,3 +8,20 @@ Given a non-negative integer represented as a non-empty array of digits, plus on
 You may assume the integer do not contain any leading zero, except the number 0 itself.
 The digits are stored such that the most significant digit is at the head of the list.
 """
+
+
+class Solution(object):
+    def plus_one(self, digits):
+        """
+        :param digits: List[int]
+        :return: List[int]
+        """
+        num = 0
+        for i in range(len(digits)):
+            num += digits[i] * pow(10, (len(digits) - i - 1))
+
+        return [int(i) for i in str(num + 1)]
+
+obj = Solution()
+print(obj.plus_one([2, 3, 4, 5]))
+print(obj.plus_one([9, 9, 9, 9]))

4d818dbcfdc40442d66eb89ca09381b4cafa8a56
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-27 14:22:16 +0800

    update

--- Basic Data Structures/stack/ReverseString.py
+++ Basic Data Structures/stack/ReverseString.py
@@ -2,34 +2,34 @@
 """
 use a list
 """
-def reverse_string(input_str):
+def reverse_str1(inputStr):
 
-	reversed_str = []
+	reversedStr = []
 
-	if len(input_str) == 0:
+	if len(inputStr) == 0:
 		return None
-	elif len(input_str) == 1:
-		reversed_str = input_str
-		return reversed_str
+	elif len(inputStr) == 1:
+		reversedStr = inputStr
+		return reversedStr
 	else:
-		for i in range(len(input_str)):
-			buff = input_str.pop()
-			reversed_str.append(buff)
-		return reversed_str
+		for i in range(len(inputStr)):
+			buff = inputStr.pop()
+			reversedStr.append(buff)
+		return reversedStr
 
 # test case
 test_str1 = []
 test_str2 = ['a']
 test_str3 = ['g', 'f', 'e', 'd', 'c', 'b', 'a']
-print(reverse_string(test_str1))
-print(reverse_string(test_str2))
-print(reverse_string(test_str3))
+print(reverse_str1(test_str1))
+print(reverse_str1(test_str2))
+print(reverse_str1(test_str3))
 
 
 """
 use a string
 """
-def reverse_str(inputStr):
+def reverse_str2(inputStr):
 
 	reversedStr = ''
 	if len(inputStr) <= 1:
@@ -43,6 +43,25 @@ def reverse_str(inputStr):
 testStr1 = ''
 testStr2 = 'a'
 testStr3 = 'gfedcba'
-print(reverse_str(testStr1))
-print(reverse_str(testStr2))
-print(reverse_str(testStr3))
+print(reverse_str2(testStr1))
+print(reverse_str2(testStr2))
+print(reverse_str2(testStr3))
+
+
+"""
+use string slicing
+"""
+
+def reverse_str3(inputStr):
+	if len(inputStr) == 0:
+		return None
+	else:
+		return inputStr[::-1]
+
+# test case
+str1 = ''
+str2 = 'a'
+str3 = 'gfedcba'
+print(reverse_str3(str1))
+print(reverse_str3(str2))
+print(reverse_str3(str3))

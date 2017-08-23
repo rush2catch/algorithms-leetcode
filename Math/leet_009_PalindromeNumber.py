8a1468264f33f58ccec50e9f37ad6156ee4dc90c
Yuan Jiang <jiangyuan1988@gmail.com>
2017-08-23 13:29:49 +0800

    leetcode

--- Math/leet_009_PalindromeNumber.py
+++ Math/leet_009_PalindromeNumber.py
@@ -11,16 +11,15 @@ Determine whether an integer is a palindrome. Do this without extra space
 class Solution(object):
 
 	def is_panlindrome(self, x):
-		if x < 0 or (x >0 and x % 10 == 0):
+		if x < 0 or (x > 0 and x % 10 == 0):
 			return False
 		num = x
 		rev = 0
 
-
 		while x != 0:
 			tail = x % 10
 			rev = rev * 10 + tail
-			x = x // 10
+			x //= 10
 
 		return num == rev
 

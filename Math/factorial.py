68622963e37f3378dff5e38dee8d54877c7ac48c
Yuan Jiang <jiangyuan1988@gmail.com>
2017-09-18 16:34:59 +0800

    leetcode

--- Math/factorial.py
+++ Math/factorial.py
@@ -5,8 +5,17 @@ def fac(n):
 		return 1
 	return n*fac(n-1)
 
+def fac_dp(n):
+	dp = [1 for i in range(n+1)]
+	for i in range(1, n+1):
+		dp[i] = dp[i-1]*i
+	return dp[-1]
+
+
 def print_fac(n):
 	for i in range(n+1):
 		print(i, fac(i))
+	for i in range(n+1):
+		print(i, fac_dp(i))
 
-print_fac(50)
+print_fac(5)

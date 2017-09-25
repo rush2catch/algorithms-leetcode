bb6cdb01b7527b73b0705a8a0a573354777be671
Yuan Jiang <jiangyuan1988@gmail.com>
2017-09-25 14:51:23 +0800

    leetcode

--- Basic Data Structures/array/leet_081_SearchinRotatedSortedArrayII.py
+++ Basic Data Structures/array/leet_081_SearchinRotatedSortedArrayII.py
@@ -1,7 +1,7 @@
-# Problem: Search in Rotated Sorted Array
+# Problem: Search in Rotated Sorted Array II
 # Difficulty: Medium
 # Category: Array
-# Leetcode 33: https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+# Leetcode 81: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
 """
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
@@ -14,27 +14,29 @@ class Solution(object):
 
 	def search(self, nums, target):
 		if len(nums) == 0:
-			return -1
+			return False
 		left = 0
 		right = len(nums) - 1
 
 		while left <= right:
 			mid = (left + right) // 2
 			if nums[mid] == target:
-				return mid
-			if nums[mid] >= nums[left]:
+				return True
+			elif nums[mid] > nums[left] or nums[mid] > nums[right]:
 				if target < nums[mid] and target >= nums[left]:
 					right = mid - 1
 				else:
 					left = mid + 1
-			if nums[mid] <= nums[right]:
+			elif nums[mid] < nums[right] or nums[mid] < nums[left]:
 				if target <= nums[right] and target > nums[mid]:
 					left = mid + 1
 				else:
 					right = mid - 1
-		return -1
+			else:
+				right -= 1
+		return False
 
 
 obj = Solution()
-arr1 = [4, 5, 6, 7, 0, 1, 2]
-print(obj.search(arr1, 7))
+arr1 = [4, 5, 6, 7, 0, 0, 0, 0, 1, 2]
+print(obj.search(arr1, 0))

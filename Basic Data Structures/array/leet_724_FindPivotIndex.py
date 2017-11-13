0ac885495e14e7c24e519081791ce2977167dfe2
Yuan Jiang <jiangyuan1988@gmail.com>
2017-11-13 20:33:31 +0800

    leetcode

---
+++ Basic Data Structures/array/leet_724_FindPivotIndex.py
@@ -0,0 +1,49 @@
+# Problem: Find Pivot Index
+# Difficulty: Easy
+# Category: Array
+# Leetcode 724: https://leetcode.com/problems/find-pivot-index/description/
+# Description:
+"""
+Given an array of integers nums, write a method that returns the "pivot" index of this array.
+We define the pivot index as the index where the sum of the numbers
+to the left of the index is equal to the sum of the numbers to the right of the index.
+If no such index exists, we should return -1.
+If there are multiple pivot indexes, you should return the left-most pivot index.
+
+Example 1:
+Input:
+nums = [1, 7, 3, 6, 5, 6]
+Output: 3
+Explanation:
+The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to the sum of numbers to the right of index 3.
+Also, 3 is the first index where this occurs.
+Example 2:
+Input:
+nums = [1, 2, 3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+"""
+
+
+class Solution(object):
+	def find_pivot(self, nums):
+		if len(nums) == 0:
+			return -1
+		pivot = -1
+		left = 0
+		right = len(nums) - 1
+		while left < right:
+			mid = (left + right) // 2
+			if sum(nums[:mid]) == sum(nums[mid+1:]):
+				pivot = mid
+				break
+			elif sum(nums[:mid]) < sum(nums[mid+1:]):
+				left = mid + 1
+			else:
+				right = mid
+		return pivot
+
+obj = Solution()
+a1 = [1, 7, 3, 6, 5, 6]
+print(obj.find_pivot(a1))
